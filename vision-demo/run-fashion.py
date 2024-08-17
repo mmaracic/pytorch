2d360e671b8290c52cf652e8a8e3bd2c4f925477
mmaracic <marijo.maracic@gmail.com>
2024-08-17 19:42:27 +0200

    Added vision FashionMNIST demo

---
+++ vision-demo/run-fashion.py
@@ -0,0 +1,81 @@
+#Test the model for FashionMNIST database
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def main():
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    batch_size = 64
+
+    # Create data loaders.
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+
+    #Define device wich will process the model
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+
+    # Define model
+    class NeuralNetwork(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.flatten = nn.Flatten()
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(28*28, 512),
+                nn.ReLU(),
+                nn.Linear(512, 512),
+                nn.ReLU(),
+                nn.Linear(512, 10)
+            )
+
+        def forward(self, x):
+            x = self.flatten(x)
+            logits = self.linear_relu_stack(x)
+            return logits
+
+    model = NeuralNetwork().to(device)
+    model.load_state_dict(torch.load("model/demo-torchvision-model2024-08-17 19:20:32.738187.pth"))
+    model.eval()
+    print(model)
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    #Test the model
+    test(test_dataloader, model, loss_fn, device)
+    print("Done!")
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+if __name__ == "__main__":
+    main()
